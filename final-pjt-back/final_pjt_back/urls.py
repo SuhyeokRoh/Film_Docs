13969ab7a18714d85a33d4137400b5d5fe2f4e2a
RohSuHyeok <roh403@naver.com>
2023-05-17 17:51:03 +0900

    Refactor: Add path 로그인 기능 구현을 위해 코드 추가

--- final-pjt-back/final_pjt_back/urls.py
+++ final-pjt-back/final_pjt_back/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('movies/', include('movies.urls')),
     path('reviews/', include('reviews.urls')),
-    path('accounts/', include('dj_rest_auth.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
 ]
