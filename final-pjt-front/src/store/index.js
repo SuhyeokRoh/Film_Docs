d6d3d6a3dd9e1452f002abc73fc65ee38513720d
주영인 <jyi8714@naver.com>
2023-05-17 17:17:54 +0900

    Refactor: axios import delete 동작 확인을 위한 axios 주석

--- final-pjt-front/src/store/index.js
+++ final-pjt-front/src/store/index.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import createPersistedState from 'vuex-persistedstate'
-import axios from 'axios'
+// import axios from 'axios'
 
 Vue.use(Vuex)
 
-const API_URL = "http://127.0.0.1:8000"
+// const API_URL = "http://127.0.0.1:8000"
 
 export default new Vuex.Store({
   plugins: [
