1ae4124110b790371fb0153fd77fee73c2b0746f
주영인 <jyi8714@naver.com>
2023-05-18 17:06:58 +0900

    Refactor: url path refactor urls.py path /2개 연속 오타 수정

--- final-pjt-back/movies/urls.py
+++ final-pjt-back/movies/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', views.movie_list, name="movie_list"),
     path('<int:movie_pk>/', views.movie_detail, name="movie_detail"),
     path('<int:movie_pk>/reviews/', views.review_create, name="review_create"),
-    path('<int:movie_pk>/reviews//<int:review_pk>/', views.review_detail, name="review_detail"),
+    path('<int:movie_pk>/reviews/<int:review_pk>/', views.review_detail, name="review_detail"),
 ]
