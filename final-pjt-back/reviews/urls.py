f52712a8f15399a40ee16dfcbba2ed79c8c775be
RohSuHyeok <roh403@naver.com>
2023-05-18 11:56:17 +0900

    Remove: Delete Review directory Review 내의 요소들을 movie에 구현함에 따라 폴더 삭제

--- final-pjt-back/reviews/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'reviews'
-urlpatterns = [
-    path('', views.review_create),
-    path('<int:review_pk>/', views.review_update_delete),
-]
