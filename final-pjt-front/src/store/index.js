eb111d67e438c8f9b1bbebc263e8ad74701f91fb
JOO YEONGIN <jyi8714@gmail.com>
2023-05-18 21:58:59 +0900

    Feat: add get Review method 리뷰 기능 구현을 위한 method 작성

--- final-pjt-front/src/store/index.js
+++ final-pjt-front/src/store/index.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import createPersistedState from 'vuex-persistedstate'
-// import axios from 'axios'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
-// const API_URL = "http://127.0.0.1:8000"
+const API_URL = "http://127.0.0.1:8000"
 
 export default new Vuex.Store({
   plugins: [
@@ -22,7 +22,10 @@ export default new Vuex.Store({
     },
     LOGOUT_USER(state) {
       state.username = null
-    }
+    },
+    GET_REVIEWS(state, reviews) {
+      state.reviews = reviews
+    },
   },
   actions: {
     Login_create_username(context, username) {
@@ -30,7 +33,19 @@ export default new Vuex.Store({
     },
     Logout_delete_username(context) {
       context.commit('LOGOUT_USER')
-    }
+    },
+    getReviews(context) {
+      axios({
+        method: 'get',
+        url: `${API_URL}/movies/${this.movie.id}/reviews/`,
+      })
+        .then((res) => {
+          context.commit('GET_REVIEWS', res.data)
+        })
+        .catch((err) => {
+        console.log(err)
+      })
+    },
   },
   modules: {
   }
