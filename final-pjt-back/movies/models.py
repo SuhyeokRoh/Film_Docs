949443d9c4895015be7f3552b5b0c06c55ef83a1
RohSuHyeok <roh403@naver.com>
2023-05-18 11:55:19 +0900

    Fead: Add Review class Review 앱을 삭제하고 Review의 class와 url, def을 movie내에 작성

--- final-pjt-back/movies/models.py
+++ final-pjt-back/movies/models.py
@@ -5,6 +5,7 @@ from django.conf import settings
 class Genre(models.Model):
     name = models.CharField(max_length=50)
 
+
 class Movie(models.Model):
     title = models.CharField(max_length=100)
     release_date = models.DateField(null=True)
@@ -16,4 +17,14 @@ class Movie(models.Model):
     poster_path = models.CharField(max_length=200)
     genres = models.ManyToManyField(Genre)
     movie_like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="movielike")
+
+
+class Review(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_reviews')
+    dislike_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='dislike_reviews')
     
--- final-pjt-back/movies/views.py
+++ final-pjt-back/movies/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import get_list_or_404, get_object_or_404
 from django.http.response import JsonResponse, HttpResponse
-from django.core import serializers
-from .serializers import MovieSerializer, MovieListSerializer
+from .serializers import MovieSerializer, MovieListSerializer, ReviewSerializer, ReviewListSerializer
 
-from .models import Movie, Genre
+from .models import Movie, Genre, Review
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -21,3 +20,39 @@ def movie_detail(request, movie_pk):
     movie = get_object_or_404(Movie, pk=movie_pk)
     serializer = MovieSerializer(movie)
     return Response(serializer.data)
+
+
+@api_view(['GET', 'POST'])
+def review_create(request, movie_pk):
+    if request.method == 'GET':
+        movie = get_object_or_404(Movie, pk=movie_pk)
+        reviews = movie.review_set.all()
+        serializer = ReviewListSerializer(reviews, many=True)
+        return Response(serializer.data)
+    else:
+        serializer = ReviewSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save(username=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def review_detail(request, movie_pk, review_pk):
+    movie = get_object_or_404(Movie, pk=movie_pk)
+    review = movie.review_set.objects(pk=review_pk)
+
+    if request.method == 'GET':
+        serializer = ReviewSerializer(review)
+        return Response(serializer.data)
+    
+    elif request.method == 'PUT':
+        if not request.user.review_set.filter(pk=review_pk).exists():
+            return Response({'detail':'권한이 없습니다.'}, status=status.HTTP_403_FORBIDDEN)
+    
+        serializer = ReviewSerializer(review, data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+    else:
+        review.delete()
+        return Response({ 'id': review_pk }, status=status.HTTP_204_NO_CONTENT)
