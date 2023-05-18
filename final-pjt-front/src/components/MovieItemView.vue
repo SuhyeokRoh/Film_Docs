1f74846f65034ebe157440024127beb5eb9bddc7
RohSuHyeok <roh403@naver.com>
2023-05-18 11:58:09 +0900

    Create: Create Review components Review에 대한 내용을 보여주기 위해 vue 생성

--- final-pjt-front/src/components/MovieItemView.vue
+++ final-pjt-front/src/components/MovieItemView.vue
@@ -5,19 +5,50 @@
     <p>{{ movie.vote_average }}</p>
     <p>{{ movie.release_date }}</p>
     <p>{{ movie.overview }}</p>
+    <ReviewListView />
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+import ReviewListView from './ReviewListView.vue'
+
+const URL = "http://127.0.0.1:8000"
 export default {
   name: 'MovieItemView',
+  components: {
+    ReviewListView,
+  },
   props: {
     movie: Object,
   },
   computed: {
     getPoster() {
-      console.log(`https://image.tmdb.org/t/p/original/${this.movie.poster_path}`)
+      // console.log(`https://image.tmdb.org/t/p/original/${this.movie.poster_path}`)
       return `https://image.tmdb.org/t/p/w500/${this.movie.poster_path}`
+    }
+  },
+  created() {
+    this.getReview()
+  },
+  methods: {
+    setToken: function() {
+      const token = localStorage.getItem("jwt")
+      const config = {
+        Authorization: `Bearer ${token}`
+      }
+      return config
+    },
+    getReview(movie_pk) {
+      axios({
+        method: 'get',
+        url: `${URL}/reviews/${movie_pk}/`,
+        headers: this.setToken()
+      })
+      .then(res => {
+        console.log(res.data)
+      })
+      .catch(err => console.log(err))
     }
   }
 }
