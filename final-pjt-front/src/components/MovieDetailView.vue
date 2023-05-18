f168d4fa0a70bf7e5524b21569ef847f961a3773
SuhyeokRoh <astrodevelop0926@gmail.com>
2023-05-18 21:42:48 +0900

    Create: Create MovieDetailView file 영화 포스터 혹은 제목 클릭 시 상세 페이지로 이동할 수 있도록 새로운 vue 파일 생성

---
+++ final-pjt-front/src/components/MovieDetailView.vue
@@ -0,0 +1,83 @@
+<template>
+  <div>
+    <h1>Movie Detail Page</h1>
+    <h3>제목 : {{ $route.params.movie.title }}</h3>
+    <img :src=getPoster >
+    <p>개봉일 : {{ $route.params.movie.release_date }}</p>
+    <p>인기도 : {{ $route.params.movie.popularity }}</p>
+    <p>투표수 : {{ $route.params.movie.vote_count }}</p>
+    <p>투표 평점 : {{ $route.params.movie.vote_average }}</p>
+    <p>줄거리 : {{ $route.params.movie.overview }}</p>
+    <p>장르 : {{ $route.params.movie.genres }}</p>
+
+    <div>
+      <label for="review">리뷰 작성 : </label>
+      <input type="text" id="review" v-model="Review" @keyup.enter="createReview">
+      <ReviewItemView 
+      v-for="review in $route.params.reviews" :key="review.id"
+      :review="review" />
+      <button @click="createReview">리뷰 작성</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import ReviewItemView from './ReviewItemView.vue'
+import axios from 'axios'
+
+const URL = "http://127.0.0.1:8000"
+export default {
+  name: "MovieDetailView",
+  data() {
+    return {
+      Review: null,
+    }
+  },
+  props: {
+    movie: {
+      type: Object,
+    },
+    reviews: {
+      type: Array,
+    }
+  },
+  components: {
+    ReviewItemView,
+  },
+  computed: {
+    getPoster() {
+      // console.log(`https://image.tmdb.org/t/p/original/${this.movie.poster_path}`)
+      return `https://image.tmdb.org/t/p/w500/${this.movie.poster_path}`
+    }
+  },
+  methods: {
+    setToken: function() {
+      const token = localStorage.getItem("jwt")
+      const config = {
+        Authorization: `Bearer ${token}`
+      }
+      return config
+    },
+    createReview: function() {
+      const user = this.$store.state.username
+      console.log(this.Review)
+
+      axios({
+        method: "post",
+        url: `${URL}/movies/${this.movie.id}/reviews/`,
+        data: { 'content' :this.Review, 'user': user},
+        headers: this.setToken()
+      })
+      .then((res) => {
+        console.log(res)
+      }).catch((err) => {
+        console.log(err)
+      })
+    },
+  }
+}
+</script>
+
+<style>
+
+</style>
