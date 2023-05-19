816dac98aed8acad2ee22d0091253d84a70994c6
주영인 <jyi8714@naver.com>
2023-05-19 15:28:39 +0900

    Fix: edit movieserializers for data serialize 장르 데이터 이름으로 반환 받기 위한 코드 작성

--- final-pjt-back/movies/serializers.py
+++ final-pjt-back/movies/serializers.py
@@ -15,26 +15,26 @@ class MovieListSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
-# class MovieSerializer(serializers.ModelSerializer):
-    
-#     genres = GenreSerializer(many=True)
-#     class Meta:
-#         model = Movie
-#         fields = "__all__"
 class MovieSerializer(serializers.ModelSerializer):
-    class GenreNameSerializer(serializers.ModelSerializer):
-        class Meta:
-            model = Genre
-            fields = ('name',)
-
-        def to_representation(self, instance):
-            return instance.name
-
-    genres = GenreNameSerializer(many=True, read_only=True)
-
+    
+    genres = GenreSerializer(many=True)
     class Meta:
         model = Movie
         fields = "__all__"
+# class MovieSerializer(serializers.ModelSerializer):
+#     class GenreNameSerializer(serializers.ModelSerializer):
+#         class Meta:
+#             model = Genre
+#             fields = ('name',)
+
+#         def to_representation(self, instance):
+#             return instance.name
+
+#     genres = GenreNameSerializer(many=True, read_only=True)
+
+#     class Meta:
+#         model = Movie
+#         fields = "__all__"
 
 class ReviewListSerializer(serializers.ModelSerializer):
     
