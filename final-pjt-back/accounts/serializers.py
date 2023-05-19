84f32f592894514858c4707dec046261afaf7a5c
RohSuHyeok <roh403@naver.com>
2023-05-19 17:55:51 +0900

    Feat: Update to show comment in profile 프로필에서 자신이 작성한 댓글 보여주기 기능 구현중

--- final-pjt-back/accounts/serializers.py
+++ final-pjt-back/accounts/serializers.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-
+from movies.serializers import ReviewListSerializer
 
 
 class UserSerializer(serializers.ModelSerializer):
+    review_set = ReviewListSerializer(many=True, read_only=True)
+
     class Meta:
         model = get_user_model()
-        fields = ('username', 'last_name', 'first_name', 'email', 'nickname',)
+        fields = ('username', 'last_name', 'first_name', 'email', 'nickname','review_set',)
         
