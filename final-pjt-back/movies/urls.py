1331c4bf95dad97ebc083079724b7811f487ab00
SuhyeokRoh <astrodevelop0926@gmail.com>
2023-05-20 22:05:52 +0900

    Feat: Add review like function 리뷰 좋아요 기능 구현을 위해 함수 생성 및 경로 추가

--- final-pjt-back/movies/urls.py
+++ final-pjt-back/movies/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('<int:movie_pk>/reviews/', views.reviewList, name="reviewList"),
     path('<int:movie_pk>/reviews/create/', views.review_create, name="review_create"),
     path('<int:movie_pk>/reviews/<int:review_pk>/', views.review_detail, name="review_detail"),
-    path('<int:movie_pk>/reviews/<int:review_pk>/update', views.review_update, name="review_update"),
+    path('<int:movie_pk>/reviews/<int:review_pk>/update/', views.review_update, name="review_update"),
+    path('<int:movie_pk>/reviews/<int:review_pk>/like/', views.review_like, name="review_like"),
 ]
