884c6f6161abfd03845754240c47980297228dc8
JOO YEONGIN <jyi8714@gmail.com>
2023-05-21 03:11:21 +0900

    Fix: fix the follow url code follow url 수정

--- final-pjt-back/accounts/urls.py
+++ final-pjt-back/accounts/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('<int:user_id>/', views.userContent, name='userContent'),
     path('<username>/profile/', views.profile, name="profile"),
+    path('<username>/profile/follow/', views.follow, name="follow"),
     # path('uniquecheck/username', views.UsernameUniqueCheck.as_view(), name='uniquecheck_username'),
 ]
