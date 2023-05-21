39624acb97afc520faced424100972fcdc3fadfd
JOO YEONGIN <jyi8714@gmail.com>
2023-05-22 01:59:37 +0900

    Create: create RecommendVoterateView.vue 평점에 의한 추천을 보여줄 추천Vue 파일 생성

---
+++ final-pjt-front/src/components/RecommendVoterateView.vue
@@ -0,0 +1,42 @@
+<template>
+  <div @click="gotoDetail">
+    <img :src=getPoster >
+    <h2>{{ movie.title }}</h2>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'RecommendVoterateView',
+  props: {
+    movie: Object,
+  },
+  data() {
+    return {
+      reviews: null,
+    }
+  },
+  computed: {
+    getPoster() {
+      return `https://image.tmdb.org/t/p/w500/${this.movie.poster_path}`
+    }
+  },
+  created() {
+    this.getReview()
+  },
+  methods: {
+    setToken: function() {
+      const token = localStorage.getItem("jwt")
+      const config = {
+        Authorization: `Bearer ${token}`
+      }
+      return config
+    },
+  }
+}
+</script>
+
+<style>
+
+</style>
