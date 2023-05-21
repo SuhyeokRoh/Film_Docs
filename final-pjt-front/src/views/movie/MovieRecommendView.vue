9f4fbdfc7ab23261be85d5615e2d334f734c1f8b
JOO YEONGIN <jyi8714@gmail.com>
2023-05-22 02:01:44 +0900

    Feat: add recommendation movies by vote_rate 평점이 높은 순으로 영화 보여주는 기능 구현

--- final-pjt-front/src/views/movie/MovieRecommendView.vue
+++ final-pjt-front/src/views/movie/MovieRecommendView.vue
@@ -1,14 +1,71 @@
 <template>
   <div>
-    <h1>
-      <p>추천 받아라!</p>
-    </h1>
+    <h1>영화 추천입니다.</h1>
+    <div>
+      <p>무작위</p>
+    </div>
+    <div>
+      <p>평점순</p>
+      <div>
+        <RecommendVoterateView v-for="movie in vote_rate_movies" :key="movie.id" 
+      :movie="movie" />
+      </div>
+    </div>
+    <div>
+      <p>인기순</p>
+    </div>
+    <div>
+      <p>좋아요순</p>
+    </div>
+    <div>
+      <p>싫어요순</p>
+    </div>
   </div>
 </template>
 
 <script>
+import RecommendVoterateView from '@/components/RecommendVoterateView'
+
+import axios from 'axios'
+const URL = "http://127.0.0.1:8000"
+
+
 export default {
   name: 'MovieRecommendView',
+  data: function() {
+    return {
+      vote_rate_movies : null
+    }
+  },
+  components: {
+    RecommendVoterateView,
+  },
+  methods: {
+    setToken: function() {
+      const token = localStorage.getItem("jwt")
+      const config = {
+        Authorization: `Bearer ${token}`
+      }
+      return config
+    },
+    
+    getRecommendMovie() {
+      axios({
+        method: 'get',
+        url: `${URL}/movies/recommend/`,
+        headers: this.setToken()
+      })
+      .then((res) => {
+        console.log(res)
+        this.vote_rate_movies = res.data
+      })
+      .catch((err) => console.log(err))
+    },
+  },
+  created() {
+    this.getRecommendMovie()
+  }
+  
 }
 </script>
 
