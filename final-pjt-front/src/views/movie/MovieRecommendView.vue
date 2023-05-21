5cb9d9feaad74ad125f7bee04adf4f9f5435a320
JOO YEONGIN <jyi8714@gmail.com>
2023-05-21 21:07:34 +0900

    Create: create MovieRecommendView.vue 영화 추천 사이트 vue파일 생성

---
+++ final-pjt-front/src/views/movie/MovieRecommendView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>
+      <p>추천 받아라!</p>
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MovieRecommendView',
+}
+</script>
+
+<style>
+
+</style>
