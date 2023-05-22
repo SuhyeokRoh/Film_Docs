b3d6aa543876f15d9e29eca0b618628bef593c6e
주영인 <jyi8714@naver.com>
2023-05-22 13:30:58 +0900

    Create: create recommend dislike vue 싫어요 순으로 정렬하는 것을 보여주기 위한 vue파일

---
+++ final-pjt-front/src/components/RecommendDislikeView.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <img :src=getPoster >
+    <h2>{{ movie.title }}</h2>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'RecommendDislikeView',
+  props: {
+    movie: Object,
+  },
+  data() {
+    return {
+      reviews: null,
+    }
+  },
+  computed: {
+    getPoster() {
+      return `https://image.tmdb.org/t/p/w500/${this.movie.poster_path}`
+    }
+  },
+  
+  methods: {
+    setToken: function() {
+      const token = localStorage.getItem("jwt")
+      const config = {
+        Authorization: `Bearer ${token}`
+      }
+      return config
+    },
+  }
+}
+</script>
+
+<style>
+
+</style>
