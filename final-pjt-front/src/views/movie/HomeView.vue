35228885595f394a87ef761bc4f4b4ad8176aa94
RohSuHyeok <roh403@naver.com>
2023-05-22 15:33:02 +0900

    Feat: Add main page component 메인 페이지에 무작위 영화 10개의 포스터 이미지를 띄움

--- final-pjt-front/src/views/movie/HomeView.vue
+++ final-pjt-front/src/views/movie/HomeView.vue
@@ -1,21 +1,44 @@
 <template>
-  <div>
+  <div v-if="movieList">
     <h1>HomePage</h1>
+    <div v-for="movie in movieList" :key="movie.id">
+      <h3>{{movie.title}}</h3>
+      <img :src=getPoster(movie)>
+    </div>
   </div>
 </template>
 
 <script>
-// import axios from 'axios'
-// import _ from 'lodash'
+import axios from 'axios'
+import _ from 'lodash'
 
-// const URL = "http://127.0.0.1:8000"
+const URL = "http://127.0.0.1:8000"
 export default {
   name: 'HomeView',
   data() {
     return {
-
+      movieList: [],
     }
   },
+  methods: {
+    getMovie(){
+      axios({
+        method: 'get',
+        url: `${URL}/movies/`,
+      })
+      .then((res) => {
+        const movie = res.data
+        this.movieList = _.sampleSize(movie, 10)
+      })
+      .catch(err => console.log(err))
+    },
+    getPoster(movie) {
+      return `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
+    }
+  },
+  created() {
+    this.getMovie()
+  }
 }
 </script>
 
