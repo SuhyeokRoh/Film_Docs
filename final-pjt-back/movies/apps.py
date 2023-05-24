91ff19afb9476f36b6c744973c7122cccdc2bba1
RohSuHyeok <roh403@naver.com>
2023-05-24 17:48:31 +0900

    Omment: Annotation Processing 장고 스케쥴러 사용을 주석 처리

--- final-pjt-back/movies/apps.py
+++ final-pjt-back/movies/apps.py
@@ -6,7 +6,7 @@ class MoviesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'movies'
 
-    def ready(self):
-        # if settings.SCHEDULER_DEFAULT:
-        from . import operator
-        operator.start()
+    # def ready(self):
+    #     # if settings.SCHEDULER_DEFAULT:
+    #     from . import operator
+    #     operator.start()
