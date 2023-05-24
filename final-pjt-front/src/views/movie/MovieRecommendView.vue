bec97a43da5a650be8b37b97770c5f025ccb3a78
RohSuHyeok <roh403@naver.com>
2023-05-24 13:39:38 +0900

    Style: Create styling 영화 추천 페이지에서 영화 포스터 스타일링 구현

--- final-pjt-front/src/views/movie/MovieRecommendView.vue
+++ final-pjt-front/src/views/movie/MovieRecommendView.vue
@@ -1,41 +1,44 @@
 <template>
   <div class="inner">
-    <h1>영화 추천입니다.</h1>
-    <div>
+    <div class="col">
       <h1>무작위</h1>
-      <div @click="gotoDetail(r_movie)" v-for="r_movie in random_movies" :key="r_movie.id">
-        <img :src="r_movie.poster_path_500" >
-        <h2>{{ r_movie.title }}</h2>
+      <div class="row wrapper">
+        <div class="ableToClick rcmlist" v-for="r_movie in random_movies" :key="r_movie.id">
+          <div class="cardrcm"  @click="gotoDetail(r_movie)">
+            <div class="frontrcm"><img :src="r_movie.poster_path_500" class="rcmPoster"></div>
+            <div class="backrcm">{{r_movie.title}}</div>
+          </div>
+        </div>
       </div>
     </div>
 
-    <div>
+    <div class="col">
       <h1>평점순</h1>
-      <div>
+      <div class="row wrapper">
         <RecommendVoterateView v-for="v_movie in vote_rate_movies" :key="v_movie.id" 
       :movie="v_movie" />
       </div>
     </div>
 
-    <div>
+    <div class="col">
       <h1>인기순</h1>
-      <div>
+      <div class="row wrapper">
         <RecommendPopularityView v-for="p_movie in popularity_movies" :key="p_movie.id" 
       :movie="p_movie" />
       </div>
     </div>
 
-    <div>
+    <div class="col">
       <h1>좋아요순</h1>
-      <div>
+      <div class="row wrapper">
         <RecommendLikeView v-for="l_movie in like_movies" :key="l_movie.id" 
       :movie="l_movie" />
       </div>
     </div>
 
-    <div>
+    <div class="col">
       <h1>싫어요순</h1>
-      <div>
+      <div class="row wrapper">
         <RecommendDislikeView v-for="d_movie in dislike_movies" :key="d_movie.id" 
       :movie="d_movie" />
       </div>
@@ -128,5 +131,51 @@ export default {
 </script>
 
 <style>
+.wrapper {
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-between;
+  height: 500px;
+  width: 100%;
+}
 
+.rcmlist{
+  position: relative;
+  width: 245.44px;
+  height: 350px;
+}
+
+.rcmPoster {
+  width: 100%;
+  height: 100%;
+}
+
+.cardrcm {
+  height: 100%;
+  width: 100%;
+  position: relative;
+  transition: 1s ;
+  transform-style: preserve-3d;
+}
+
+
+.frontrcm, .backrcm {
+  position: absolute;
+  width: 100%; 
+  height: 100%;
+  backface-visibility: hidden;
+}
+
+.rcmlist:hover .cardrcm {
+  transform: rotateY(180deg);
+}
+
+.backrcm {
+  background-color: black;
+  color:white;
+  border-radius: 7px;
+  line-height: 350px;
+  font-size: 17px;
+  transform: rotateY(180deg);
+}
 </style>
