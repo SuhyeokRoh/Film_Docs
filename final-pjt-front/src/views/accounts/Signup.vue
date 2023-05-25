5e5cbd56ef3abe3ab94e8767bfaf9fe3205b9d2f
주영인 <jyi8714@naver.com>
2023-05-25 15:50:30 +0900

    Style: change signup box 회원 가입 박스 및 폰트 수정

--- final-pjt-front/src/views/accounts/Signup.vue
+++ final-pjt-front/src/views/accounts/Signup.vue
@@ -4,34 +4,34 @@
 
     <h1 class="sign_up">SignUp</h1>
     <div id="id">
-      <label for="username">사용자 계정 : </label>
+      <label for="username">ID  </label>
       <input type="text" id="username" v-model="userdata.username" />
     </div>
     <div id="lastname">
-      <label for="last_name">성 : </label>
+      <label for="last_name">성  </label>
       <input type="text" id="last_name" v-model="userdata.last_name" />
     </div>
     <div id="firstname">
-      <label for="first_name">이름 : </label>
+      <label for="first_name">이름  </label>
       <input type="text" id="first_name" v-model="userdata.first_name" />
     </div>
     <div id="nickname">
-      <label for="nickname">닉네임 : </label>
+      <label for="nickname">닉네임  </label>
       <input type="text" id="nickname" v-model="userdata.nickname" />
     </div>
     <div id="email">
-      <label for="email">e-mail : </label>
+      <label for="email">e-mail  </label>
       <input type="text" id="email" v-model="userdata.email" />
     </div>
     <div id="pw">
-      <label for="password">비밀번호 : </label>
+      <label for="password">PW  </label>
       <input type="password" id="password" v-model="userdata.password" />
     </div>
     <div id="pwcf">
-      <label for="passwordConfirm">비밀번호 확인:</label>
+      <label for="passwordConfirm">PW 확인</label>
       <input type="password" id="passwordConfirm" v-model="userdata.passwordConfirm" @keyup.enter="signup" />
     </div>
-    <button @click="signup">회원 가입</button>
+    <button class="btn-5" @click="signup">회원 가입</button>
     </div>
   </div>
 </template>
@@ -85,20 +85,15 @@ export default {
 
 <style>
   #signupbox {
-    height: 800px;
+    height: 750px;
     display: flex;
     justify-content: center;
     align-items: center;
-
     margin:0;
-    
-    font:600 16px/18px 'Open Sans',sans-serif;
   }
 
   #signup {
-    color:#6a6f8c;
-    /* background:#c8c8c8; */
-    font:600 16px/18px 'Open Sans',sans-serif;
+    border-radius: 3px;
     box-shadow: 5px 5px 5px 5px #0f023b;
     width: 500px;
     height: 60%;
@@ -108,4 +103,58 @@ export default {
     align-items: center;
     box-sizing: border-box;
   }
+  #signup label {
+  display: inline-block;
+  width: 120px; /* Adjust the width to your preference */
+  margin-right: 10px; /* Adjust the right margin to create spacing */
+}
+
+.btn-5 {
+  width: 130px;
+  height: 40px;
+  line-height: 42px;
+  padding: 0;
+  font-weight: bold;
+  border-radius: 5px;
+  border: none;
+  background: rgb(228, 215, 39);
+background: linear-gradient(0deg, rgb(228, 215, 39) 0%, rgb(236, 230, 135) 100%);
+}
+.btn-5:hover {
+  color: #f0094a;
+  background: transparent;
+   box-shadow:none;
+}
+.btn-5:before,
+.btn-5:after{
+  content:'';
+  position:absolute;
+  top:0;
+  right:0;
+  height:2px;
+  width:0;
+  background: #f0de3f;
+  box-shadow:
+   -1px -1px 5px 0px #fff,
+   7px 7px 20px 0px #0003,
+   4px 4px 5px 0px #0002;
+  transition:400ms ease all;
+}
+.btn-5:after{
+  right:inherit;
+  top:inherit;
+  left:0;
+  bottom:0;
+}
+.btn-5:hover:before,
+.btn-5:hover:after{
+  width:100%;
+  transition:800ms ease all;
+}
+
+
+#signup input {
+  margin-bottom: 10px; /* Adjust the bottom margin to create spacing */
+}
+
 </style>
